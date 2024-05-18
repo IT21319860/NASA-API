7d14c2b01d9455ea130638df09062ca8f75c4b75
IT21319860 <100059519+IT21319860@users.noreply.github.com>
2024-05-18 15:05:17 +0530

    1

--- Frontend/client/frontend-NASA-API/src/pages/SignIn.jsx
+++ Frontend/client/frontend-NASA-API/src/pages/SignIn.jsx
@@ -27,7 +27,7 @@ export default function SignIn() {
       dispatch(signInStart());
       // setLoading(true);
       // setErrorMessage(null);
-      const res = await fetch('/api/auth/signin', {
+      const res = await fetch('https://nasa-api-v2.onrender.com/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
